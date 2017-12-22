b93ede889d3e3b273580dd3ac7dd9e94ba897fae
bernard <bernard@traveloka.com>
2017-12-22 15:11:46 +0700

    refactor code to be reusable

--- cloudwatch_logs_cleaner/main.py
+++ cloudwatch_logs_cleaner/main.py
@@ -15,6 +15,14 @@ def retry_on_request_limit_exceed(e):
 def lambda_handler(event, context):
     logs = boto3.client('logs')
     describe_all_log_groups(logs)
+    unused_loggroups, used_loggroups = describe_all_log_groups(logs)
+    unused_logstreams = describe_log_streams(logs, used_loggroups)
+    for loggroup in unused_loggroups:
+        print(loggroup)
+    for logstream in unused_logstreams:
+        print(logstream)
+#    delete_log_group(unused_loggroups)
+#    delete_log_stream(unused_logstreams)
 
 
 def describe_all_log_groups(logs):
@@ -35,7 +43,7 @@ def describe_all_log_groups(logs):
         else:
             used_loggroups.append(loggroup['logGroupName'])
 
-    describe_log_streams(logs, used_loggroups)
+    return unused_loggroups, used_loggroups
 
 
 @retry(retry_on_exception=retry_on_request_limit_exceed, wait_random_min=4,
@@ -57,11 +65,11 @@ def describe_log_streams(logs, loggroups):
                 raw_data = {"logGroupName": loggroupname,
                             "logStreamName": logstream['logStreamName']}
                 unused_logstreams.append(raw_data)
-
-    for logstream in unused_logstreams:
-        print(logstream)
+    return unused_logstreams
 
 
+@retry(retry_on_exception=retry_on_request_limit_exceed, wait_random_min=4,
+       wait_random_max=8, stop_max_attempt_number=5)
 def delete_log_group(logs, loggroups):
     for loggroup in loggroups:
         try:
@@ -70,8 +78,19 @@ def delete_log_group(logs, loggroups):
             )
         except Exception, e:
             print(e)
-# def delete_log_stream(logs, loggroups, logstreams):
 
+
+@retry(retry_on_exception=retry_on_request_limit_exceed, wait_random_min=4,
+       wait_random_max=8, stop_max_attempt_number=5)
+def delete_log_stream(logs, logstreams):
+    for logstream in logstreams:
+        try:
+            logs.delete_log_stream(
+                logGroupName=logstream['logGroupName'],
+                logStreamName=logstream['logStreamName']
+            )
+        except Exception, e:
+            print(e)
 
 if __name__ == "__main__":
     lambda_handler("asdf", "asdf")
